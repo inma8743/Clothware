3f0e2a6deec3aad109a49e18b586d5ef88cee11c
lee <inma8743@gmail.com>
2024-02-02 17:29:14 +0900

    adopt shade_smooth

--- exports_bpy.py
+++ exports_bpy.py
@@ -9,15 +9,7 @@ def exports_3dModeling(product_idx):
     obj_file_path = os.path.join('./uploads/stylechain/product', '3d', product_idx, 'digital_TShirt.obj')
 
     # 기존에 불러온 모든 객체 제거
-    bpy.ops.object.select_all(action='SELECT')
-    bpy.ops.object.delete()
-
-    # face OBJ 파일 불러오기
-    bpy.ops.import_scene.obj(filepath=obj_file_path)
-
-    bpy.ops.object.select_all(action='SELECT')
-    # 불러온 객체에 쉐이드 스무스 적용
-    bpy.ops.object.shade_smooth()
+    bpy.ops.wm.read_factory_settings(use_empty=True)
 
     material = bpy.data.materials.new(name="default_mat")
     material.use_nodes = True
@@ -27,15 +19,19 @@ def exports_3dModeling(product_idx):
         texImage.image = bpy.data.images.load(os.path.join(export_file_path, 'texture.png'))
         material.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
 
-    # 불러온 객체에 재질 적용
-    for obj in bpy.context.selected_objects:
-        if obj.type == 'MESH':
-            if len(obj.data.materials) == 0:
-                obj.data.materials.append(material)
-            else:
-                obj.data.materials[0] = material
+    # face OBJ 파일 불러오기
+    bpy.ops.import_scene.obj(filepath=obj_file_path)
 
+    # 마지막으로 불러온 객체 선택
+    clothes_obj = bpy.context.selected_objects[0]
+    # 머티리얼 설정
+    clothes_obj.data.materials[0] = material
 
+    mesh_object = bpy.data.objects["tshirt"]  # 메쉬 오브젝트 이름으로 대체
+    bpy.context.view_layer.objects.active = mesh_object
+    mesh_object.select_set(True)
+
+    bpy.ops.object.shade_smooth()
 
     bpy.ops.export_scene.fbx(filepath=os.path.join(export_file_path, f"{product_idx}.fbx"))
     bpy.ops.export_scene.gltf(filepath=os.path.join(export_file_path, f"{product_idx}.glb"), export_format="GLB")
